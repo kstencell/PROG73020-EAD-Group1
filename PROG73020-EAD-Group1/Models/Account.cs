41577f82b00760da5b00f5a0b3ccf3309182a208
karl <karldstencell@gmail.com>
2023-02-11 07:57:35 -0500

    Add project files.

---
+++ PROG73020-EAD-Group1/Models/Account.cs
@@ -0,0 +1,14 @@
+﻿
+
+
+namespace PROG73020_EAD_Group1.Models
+{
+    public class Account
+    {
+        public int AccountID { get; set; }
+        private string Password { get; set; }
+        public string Name { get; set; }
+        public string Bio { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ PROG73020-EAD-Group1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PROG73020_EAD_Group1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
